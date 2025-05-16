4d3654b704444095f0bab28a38fd47079df7c890
finntastic <179311210+finntaztic@users.noreply.github.com>
2025-05-16 14:22:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+
+  //dialog form 
+  const body = document.querySelector('body')
+  const dialog = document.createElement('dialog')
+  const closeBtn = document.createElement ('button')
+  closeBtn.textContent = 'X';
+  const p = document.createElement('p')
+  p.textContent = "Add the book you read 😍";
+  body.appendChild(dialog)
+  
+  dialog.appendChild(closeBtn)
+  dialog.appendChild(p)
+
+
+
+
 function GameBoard (){
   const cell = 9;
   const board = [];
@@ -42,6 +58,8 @@ function GameBoard (){
     console.log(boardCell)
   };
 
+  
+
   const display = () => {
     const body = document.querySelector('body');
     const container = document.createElement('div');
@@ -58,9 +76,10 @@ function GameBoard (){
       if (item.getValue() == 'X' || item.getValue() == 'O') {
         let textNode = document.createTextNode(item.getValue());
         div.appendChild(textNode);
-      }
+      } else return;
     });
   };
+
 
   return {
     getBoard,
@@ -99,6 +118,8 @@ function GameController (
       token: 'O'
     }
   ];
+
+
   let activePlayer = players [0];
   const switchPlayerTurn = () => {
     activePlayer = activePlayer === players [0] ? players [1] : players [0]
@@ -193,198 +214,9 @@ function GameController (
 GameController()
 
 
-
-///working code before changing value of 1 to x
-// function GameBoard (){
-//   const cell = 9;
-//   const board = [];
-
-//   for (let i = 0; i < cell; i++){
-//     board.push(Cell());
-//   }
-
-//   const getBoard = () => board;
-
-//   const dropToken = (index, player) => {
-//     if (board[index].getValue() === 0){
-//       board[index].addToken(player)
-//     } else return;
-//   }
-
-//   const checkWinner = () => {
-//     if ((board[0].getValue() === 1 && board[3].getValue() === 1 && board[6].getValue() === 1)
-//       || (board[1].getValue() === 1 && board[4].getValue() === 1 && board[7].getValue() === 1)
-//       || (board[2].getValue() === 1 && board[5].getValue() === 1 && board[8].getValue() === 1)
-//       || (board[0].getValue() === 1 && board[4].getValue() === 1 && board[8].getValue() === 1)
-//       || (board[2].getValue() === 1 && board[4].getValue() === 1 && board[6].getValue() === 1)){
-//       console.log("Player 1 wins this round 🎉")
-//     } else if ((board[0].getValue() === 2 && board[3].getValue() === 2 && board[6].getValue() === 2)
-//       || (board[1].getValue() === 2 && board[4].getValue() === 2 && board[7].getValue() === 2)
-//       || (board[2].getValue() === 2 && board[5].getValue() === 2 && board[8].getValue() === 2)
-//       || (board[0].getValue() === 2 && board[4].getValue() === 2 && board[8].getValue() === 2)
-//       || (board[2].getValue() === 2 && board[4].getValue() === 2 && board[6].getValue() === 2)){
-//       console.log("Player 2 wins this round 🎉🎉")
-//     }
-//     else if (
-//       board[0].getValue() !== 0 && board[1].getValue() !== 0 && board[2].getValue() !== 0
-//       && board[3].getValue() !== 0 && board[4].getValue() !== 0 && board[5].getValue() !== 0
-//       && board[6].getValue() !== 0 && board[7].getValue() !== 0 && board[8].getValue() !== 0
-//     ){
-//       console.log("It's a tie")
-//     } else return;
-//   }
-
-//   const printBoard = () => {
-//     const boardCell = board.map((cell => cell.getValue()));
-//     console.log(boardCell)
-//   };
-
-//   const display = () => {
-//     const body = document.querySelector('body');
-//     const container = document.createElement('div');
-//     container.id = 'container';
-//     body.appendChild(container);
-
-//     // const board = getBoard();
-
-//     board.forEach((item, index) => {
-//       const div = document.createElement('div');
-//       div.className = `boxes-${index + 1}`;
-//       container.appendChild(div);
-
-//       if (item.getValue() == '1' || item.getValue() == '2') {
-//         let textNode = document.createTextNode(item.getValue());
-//         div.appendChild(textNode);
-//       }
-//     });
-//   };
-
-//   return {
-//     getBoard,
-//     dropToken,
-//     printBoard,
-//     checkWinner,
-//     display
-//   };
-// }
-
-// function Cell (){
-//   let value = 0;
-//   const addToken = (player) => {
-//     value = player;
-//   };
-//   const getValue = () => value;
-
-//   return {
-//     addToken,
-//     getValue
-//   };
-//   }
-
-// function GameController (
-//   playerOneName = 'Player 1',
-//   playerTwoName = 'Player 2'
-// ){
-//   const board = GameBoard();
-//   const players = [
-//     {
-//       name: playerOneName,
-//       token: 1
-//     },
-//     {
-//       name: playerTwoName,
-//       token: 2
-//     }
-//   ];
-//   let activePlayer = players [0];
-//   const switchPlayerTurn = () => {
-//     activePlayer = activePlayer === players [0] ? players [1] : players [0]
-
-//   };
-//   const getActivePlayer = () => activePlayer;
-//   const printNewRound = () => {
-//     board.printBoard();
-//     console.log(`${getActivePlayer().name}'s turn.`)
-//   };
-
-//   board.display(); //initial display
-
-
-//   const playRound = (index) => {
-//     console.log(`Marking ${getActivePlayer().name}'s token into index: ${index}`);
-//     board.dropToken(index, getActivePlayer().token);
-
-//     switchPlayerTurn();
-//     printNewRound();
-//     board.checkWinner();
-//     const container = document.getElementById('container');
-//     container.remove(); //removes initial display
-//     board.display(); // display the updated board
-//     getIndex()
-//   };
-//     getIndex()
-//     printNewRound();
-
-//     function getIndex (){
-//       let boxes = document.querySelectorAll ('div[class^="boxes-"]');
-
-//         boxes.forEach((box) => {
-//           box.addEventListener('click', () => {
-//             console.log('box is clicked')
-//             let index;
-
-//             if (box.className == 'boxes-1'){
-//               index = '0';
-//               playRound(index);
-//             } else if (box.className == 'boxes-2'){
-//               index = '1';
-//               console.log (index)
-//               console.log ('box is clicked');
-//               playRound(index)
-//             } else if (box.className == 'boxes-3'){
-//               index = '2';
-//               console.log (index)
-//               console.log ('box is clicked');
-//               playRound(index)
-//             } else if (box.className == 'boxes-4'){
-//               index = '3';
-//               console.log (index)
-//               console.log ('box is clicked');
-//               playRound(index)
-//             } else if (box.className == 'boxes-5'){
-//               index = '4';
-//               console.log (index)
-//               console.log ('box is clicked');
-//               playRound(index)
-//             } else if (box.className == 'boxes-6'){
-//               index = '5';
-//               console.log (index)
-//               console.log ('box is clicked');
-//               playRound(index)
-//             } else if (box.className == 'boxes-7'){
-//               index = '6';
-//               console.log (index)
-//               console.log ('box is clicked');
-//               playRound(index)
-//             } else if (box.className == 'boxes-8'){
-//               index = '7';
-//               console.log (index)
-//               console.log ('box is clicked');
-//               playRound(index)
-//             } else if (box.className == 'boxes-9'){
-//               index = '8';
-//               console.log (index)
-//               console.log ('box is clicked');
-//               playRound(index)
-//             } 
-//           })
-//           })
-//     }
-//   return {
-//     playRound,
-//     getActivePlayer,
-//     printNewRound
-//   };
-// };
-
-// GameController()
+//backlog
+//1. gameOver , shouldnt allow to add turn to the box when theres winner -- idk  its ok 
+//2. shouldnt be able to click on the occupied box so player's turn doesnt get counted
+//3. reset button
+//4. allow players to add their name
+//5. start game button
